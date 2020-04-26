2f80d8e747adb235f78003552667525f7fe5d6db
kaiorr <kaio.rocharibeiro@gmail.com>
2020-04-26 08:56:34 -0300

    chore: finalizando desafio da semana

--- 2-semana/scripts.js
+++ 2-semana/scripts.js
@@ -12,6 +12,7 @@ const initialColors = {
     bgHeader: getStyle(html, "--bg-header"),
     colorText: getStyle(html, "--color-text"),
     colorHeadings: getStyle(html, "--color-headings"),
+    boxShadow: getStyle(html, "--box-shadow"),
     gradientLeft: getStyle(html, "--gradient-left"),
     gradientRight: getStyle(html, "--gradient-right")
     
@@ -21,6 +22,7 @@ const darkMode = {
     bg: "#000",
     bgHeader: "#1a1a1a",
     colorText: "#fafafa",
+    boxShadow: "rgba(212, 212, 212, 0.157)",
     colorHeadings: "#f0f0f0",
     gradientLeft: "#ff8a00",
     gradientRight: "#e52e71" 
