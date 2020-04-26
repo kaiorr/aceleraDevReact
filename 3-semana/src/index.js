b1164a42a5a4393afec7df9f0ce689a444ec5896
kaiorr <kaio.rocharibeiro@gmail.com>
2020-04-26 10:07:47 -0300

    feat: finalizado função que calcula os valores da série de fibonacci

--- 3-semana/src/index.js
+++ 3-semana/src/index.js
@@ -1,8 +1,22 @@
 'use strict'
 
-const fibonacci = () => null
+const fibonacci = (qtd = 350) => {
+    const numeros = Array.from(Array(qtd + 1).keys());
+    const result = [0, 1];
+    let index = 1;
+    numeros.forEach(item => {
+        if ((result[index - 1] + result[index]) === item) {
+            result.push(item);
+            index++;
+        }
+    })
+    return result;
+}
 
-const isFibonnaci = (num) => null
+const isFibonnaci = (num) => {
+    const result = fibonacci(num + 1);
+    return result.includes(num);
+}
 
 module.exports = {
     fibonacci,
