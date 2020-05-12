478182072713c91747b28ba15f797c7967fcf299
kaiorr <kaio.rocharibeiro@gmail.com>
2020-05-12 19:56:42 -0300

    feat: componentes de contato e filtro do contato finalizados

--- 5-semana/src/components/Contact.jsx
+++ 5-semana/src/components/Contact.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
 
-class Contact extends React.Component {
-  render() {
-    return (<div />);
-  }
+function Avatar(props) {
+  
+  return (
+
+  <span className='contact__avatar'>
+    <img src={props.image} alt={props.name}></img> 
+  </span> 
+
+  )
 }
 
-export default Contact;
+function TextContact(props) {
+  return (
+  <span className={'contact__data'}>{props.value}</span>
+  )
+}
+
+export default function Contact(props) {
+
+  return (
+
+    <article className='contact' data-testid='contact'>
+
+      <Avatar image={props.data.avatar} name={props.data.name} />
+      <Avatar value={props.data.name} />
+      <Avatar value={props.data.phone} />
+      <Avatar value={props.data.country} />
+      <Avatar value={props.data.admissionDate} />
+      <Avatar value={props.data.company} />
+      <Avatar value={props.data.department} />
+      
+    </article>
+
+  )
+
+}
--- 5-semana/src/components/Contacts.jsx
+++ 5-semana/src/components/Contacts.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 
 class Contacts extends React.Component {
+
 	render() {
-		return (<div />);
+
+		return (
+
+				<div className="container" data-testid="contacts">
+					<section className="contacts">
+						<article className="contacts">
+							<span className="contact__avatar" />
+							<span className="contact__data">Nome</span>
+							<span className="contact__data">Telefone</span>
+							<span className="contact__data">País</span>
+							<span className="contact__data">Admissão</span>
+							<span className="contact__data">Empresa</span>
+							<span className="contact__data">Departamentos</span>
+						</article>
+						{this.props.children}
+					</section>
+				</div>
+
+		);
+
 	}
 }
 
