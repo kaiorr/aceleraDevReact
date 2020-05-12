c70d69e49f142d8f14dacbdc03333766f3f5d78a
kaiorr <kaio.rocharibeiro@gmail.com>
2020-05-12 18:38:17 -0300

    feat: criado novo componente para a lógica da busca e tbm arquivo lottie
    	Arquivo loading.json é uma arquivo que vai atuar na transição da busca para api, demonstrando o carregamento e acesso a API.

---
+++ 5-semana/src/components/Button.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+class Button extends React.Component {
+  setClassname() {
+
+    if (this.props.state.selection === this.props.value) {
+
+      return this.props.state.direction === 'down' ? 'fas fa-sort-down' : 'fas fa-sort-up'
+    
+    } else {
+ 
+      return 'fas fa-sort-down'
+
+    }
+  }
+
+  render() {
+
+    return (
+
+      <button
+        className={tihs.props.state.selection === this.props.value ? 'filters__item is-selected' : 'filters__item'}
+        onClick={(event, field) => this.props.HandleClick(event, this.props.value)}
+      >
+        {this.props.name}
+        <i
+          className={this.setClassname()}
+        />
+      </button>
+    );
+  }
+}
+
+export default Button
