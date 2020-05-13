19b8001828f9c772999db068baf76fb6df8f375f
kaiorr <kaio.rocharibeiro@gmail.com>
2020-05-13 20:03:47 -0300

    fix: componente foi refeito devido inconsistência apresentada no search

--- 5-semana/src/components/Filters.jsx
+++ 5-semana/src/components/Filters.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import Button from './Button'
-import './Filters.scss';
-class Filters extends React.Component {
 
+import Button from './Button';
+
+import './Filters.scss';
+
+class Filters extends React.Component {
+	
 	constructor(props) {
-		super(props)
+		super(props);
 		this.state = {
 			selection: null,
 			pastSelection: null,
@@ -13,68 +16,68 @@ class Filters extends React.Component {
 	}
 
 	reverseDirection() {
-		const newDirection = this.state.direction === 'down' ? 'up' : 'down'
-		return newDirection
-	}
+        const newDirection = this.state.direction === 'down' ? 'up' : 'down'
+        return newDirection
+    }
 
 	selectField(event, field) {
-		event.preventDefault()
+		event.preventDefault();
 		const newDirection = (this.state.selection === field) ? this.reverseDirection() : 'down'
 		this.setState({
 			selection: field,
 			pastSelection: this.state.selection,
 			direction: newDirection
-		})
-		this.props.HandleClick(event, field, newDirection)
+		});
+		this.props.HandleClick(event, field, newDirection);
 	}
-
+	
 	render() {
 		return (
+
 			<div className="container" data-testid="filters">
 				<section className="filters">
+
 					<div className="filters__search" onChange={event => this.props.HandleChange(event.target.value)}>
+						<input type="text" className="filters__search__input" placeholder="Pesquisar" />
 						<button className="filters__search__icon">
-							<i className="fa fa-search" />
+						<i className="fa fa-search"/>
 						</button>
 					</div>
-
-				<Button 
-					name='Nome'
-					value='name'
-					state={this.state}
-					HandleClick={(event, field) => this.selectField(event, field)}
-				/>
-
-				<Button
-					name='Pais'
-					value='country'
-					state={this.state}
-					HandleClick={(event, field) => this.selectField(event, field)}
-				/>
-
-				<Button
-					name='Empresa'
-					value='company'
-					state={this.state}
-					HandleClick={(event, field) => this.selectField(event, field)}
-				/>
-
-				<Button
-					name='Departamento'
-					value='department'
-					state={this.state}
-					HandleClick={(event, field) => this.selectField(event, field)}
-				/>
-
-				<Button
-					name='Data de admissão'
-					value='admissionDate'
-					state={this.state}
-					HandleClick={(event, field) => this.selectField(event, field)}
-				/>
-
+		
+					<Button 
+						name='Nome'
+						value='name'
+						state={this.state}
+						HandleClick={(event, field) => this.selectField(event, field)}
+					/>
+					<Button
+						name='País'
+						value='country'
+						state={this.state}
+						HandleClick={(event, field) => this.selectField(event, field)}
+					/>
+					<Button
+						name='Empresa'
+						value='company'
+						state={this.state}
+						HandleClick={(event, field) => this.selectField(event, field)}
+					/>
+					<Button
+						name='Departamento'
+						value='department'
+						state={this.state}
+						HandleClick={(event, field) => this.selectField(event, field)}
+					/>
+					<Button
+						name='Data de admissão'
+						value='admissionDate'
+						state={this.state}
+						HandleClick={(event, field) => this.selectField(event, field)}
+					/>
+	
 				</section>
 			</div>
+
 		);
 	}
 }
